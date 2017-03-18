c6f1aff7a430f59296427b4813c9e98d0dd5be0e
Josh Levinger <josh@levinger.net>
2017-03-17 17:52:36 -0700

    improve tests to show changed CDs

--- test.py
+++ test.py
@@ -1,30 +1,64 @@
 import utils
+import collections
 
 def list_key_set(data, key):
-	s = set()
-	for d in data:
-		try:
-			s.add(d[key])
-		except KeyError:
-			print d
-			break
-	return s
+    s = set()
+    for d in data:
+        try:
+            s.add(d[key])
+        except KeyError:
+            print d
+            break
+    return s
+
+def list_key_values(data, key):
+    c = collections.defaultdict(list)
+    for d in data:
+        try:
+            c[d[key]].append(d)
+        except KeyError:
+            print d
+            break
+    return c
 
 if __name__ == "__main__":
-	print "\t new, old"
+    print "\t new, old"
 
-	new_data = utils.load_csv_columns('zccd.csv')
-	old_data = utils.load_csv_columns('raw/old_sunlight_districts.csv')
-	print "length", len(new_data), len(old_data)
+    new_data = utils.load_csv_columns('zccd.csv')
+    old_data = utils.load_csv_columns('raw/old_sunlight_districts.csv')
+    print "length", len(new_data), len(old_data)
+    print
 
-	new_zctas = list_key_set(new_data, 'zcta')
-	old_zctas = list_key_set(old_data, 'zipcode')
-	print "zctas", len(new_zctas), len(old_zctas)
-	print "new missing", old_zctas.difference(new_zctas)
-	print "old missing", new_zctas.difference(old_zctas)
+    new_states = list_key_set(new_data, 'state_abbr')
+    old_states = list_key_set(old_data, 'state')
+    print "states", len(new_states), len(old_states)
+    assert len(old_states.difference(new_states)) == 0
+    assert len(new_states.difference(old_states)) == 0
+    print
 
-	new_states = list_key_set(new_data, 'state_abbr')
-	old_states = list_key_set(old_data, 'state')
-	print "states", len(new_states), len(old_states)
-	print "new missing", old_states.difference(new_states)
-	print "old missing", new_states.difference(old_states)
+    new_zctas = list_key_set(new_data, 'zcta')
+    old_zctas = list_key_set(old_data, 'zipcode')
+    print "ZCTAs", len(new_zctas), len(old_zctas)
+    # we should not be missing any old zctas
+    assert len(old_zctas.difference(new_zctas)) == 0
+    print "added", new_zctas.difference(old_zctas)
+    print
+
+    new_zcta_list = list_key_values(new_data, 'zcta')
+    old_zcta_list = list_key_values(old_data, 'zipcode')
+    cds_changed = 0
+    states_changed = set()
+    for (n, l) in sorted(new_zcta_list.items()):
+        new_cd_set = list_key_set(l, 'cd')
+        old_cd_set = list_key_set(old_zcta_list[n], 'house_district')
+        if old_cd_set.symmetric_difference(new_cd_set):
+            cds_changed += 1
+            new_state = list_key_values(l,'state_abbr')
+            old_state = list_key_values(old_zcta_list[n], 'state')
+            print "%s was %s-%s now %s-%s" % (n, ','.join(old_state), ','.join(old_cd_set), ','.join(new_state), ','.join(new_cd_set))
+            states_changed.update(new_state.keys())
+            states_changed.update(old_state.keys())
+
+    print "CDs changed",  cds_changed
+    print "from states", states_changed
+    print
