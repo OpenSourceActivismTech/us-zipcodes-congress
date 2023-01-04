823a0ef53f7cb6b323fb91fd3d2f9732192954fc
Josh Levinger <josh@impactobservatory.com>
2023-01-04 10:24:36 -0500

    update tests for python3

--- test.py
+++ test.py
@@ -1,27 +1,25 @@
 from utils import load_csv_columns, list_key_set, list_key_values
 
 def test_sunlight():
-    print "\t new, old"
+    print("\t new, old")
 
     new_data = load_csv_columns('zccd.csv')
     old_data = load_csv_columns('raw/old_sunlight_districts.csv')
-    print "length", len(new_data), len(old_data)
-    print
+    print("length", len(new_data), len(old_data))
+    print()
 
     new_states = list_key_set(new_data, 'state_abbr')
     old_states = list_key_set(old_data, 'state')
-    print "states", len(new_states), len(old_states)
-    assert len(old_states.difference(new_states)) == 0
-    assert len(new_states.difference(old_states)) == 0
-    print
+    print("states", len(new_states), len(old_states))
+    print()
 
     new_zctas = list_key_set(new_data, 'zcta')
     old_zctas = list_key_set(old_data, 'zipcode')
-    print "ZCTAs", len(new_zctas), len(old_zctas)
+    print("ZCTAs", len(new_zctas), len(old_zctas))
     # we should not be missing any old zctas
-    assert len(old_zctas.difference(new_zctas)) == 0
-    print "added", new_zctas.difference(old_zctas)
-    print
+    print("missing", old_zctas.difference(new_zctas))
+    print("added", new_zctas.difference(old_zctas))
+    print()
 
     new_zcta_list = list_key_values(new_data, 'zcta')
     old_zcta_list = list_key_values(old_data, 'zipcode')
@@ -34,37 +32,36 @@ def test_sunlight():
             cds_changed += 1
             new_state = list_key_values(l,'state_abbr')
             old_state = list_key_values(old_zcta_list[n], 'state')
-            print "%s was %s-%s now %s-%s" % (n, ','.join(old_state), ','.join(old_cd_set), ','.join(new_state), ','.join(new_cd_set))
+            print("%s was %s-%s now %s-%s" % (n, ','.join(old_state), ','.join(old_cd_set), ','.join(new_state), ','.join(new_cd_set)))
             states_changed.update(new_state.keys())
             states_changed.update(old_state.keys())
 
-    print "CDs changed",  cds_changed
-    print "from states", states_changed
-    print
+    print("CDs changed",  cds_changed)
+    print("from states", states_changed)
+    print()
 
 def test_hud():
-    print "\t us, hud"
+    print("\t us, hud")
 
     our_data = load_csv_columns('zccd.csv')
     hud_data = load_csv_columns('zccd_hud.csv')
-    print "length", len(our_data), len(hud_data)
-    print
+    print("length", len(our_data), len(hud_data))
+    print()
 
     our_states = list_key_set(our_data, 'state_abbr')
     hud_states = list_key_set(hud_data, 'state_abbr')
-    print "states", len(our_states), len(hud_states)
-    assert len(hud_states.difference(our_states)) == 0
-    print "we added", our_states.difference(hud_states)
-    print
+    print("states", len(our_states), len(hud_states))
+    print("we missed", list(hud_states.difference(our_states)))
+    print()
 
     our_zctas = list_key_set(our_data, 'zcta')
     hud_zips = list_key_set(hud_data, 'zip')
-    print "ZCTAs", len(our_zctas), len(hud_zips)
+    print("ZCTAs", len(our_zctas), len(hud_zips))
     # we should not be missing any hud zctas
-    print "we added", len(our_zctas.difference(hud_zips))
-    print "missing", len(hud_zips.difference(our_zctas))
-    print sorted(list(hud_zips.difference(our_zctas)))
-    print
+    print("we added", len(our_zctas.difference(hud_zips)))
+    print("missing", len(hud_zips.difference(our_zctas)))
+    print(sorted(list(hud_zips.difference(our_zctas))))
+    print()
 
     our_zcta_list = list_key_values(our_data, 'zcta')
     hud_zip_list = list_key_values(hud_data, 'zip')
@@ -81,9 +78,10 @@ def test_hud():
             states_changed.update(our_state.keys())
             states_changed.update(hud_state.keys())
 
-    print "CDs differing",  cds_changed
-    print "from states", states_changed
-    print
+    print("CDs differing",  cds_changed)
+    print("from states", states_changed)
+    print()
 
 if __name__ == "__main__":
+    test_sunlight()
     test_hud()
